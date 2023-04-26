1ccec1500078a912e92400a95a5c8a955cca221f
Nathan Ruer <nath.ruer@gmail.com>
2023-04-26 15:18:09 +0800

    first commit

---
+++ components/Meta.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Meta() {
+  return (
+    <Head>
+      <title>Crypto Price Tracker App</title>
+
+      {/* App Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
+export default Meta;
